bf950b28e8194a7b642ecfc4905a8a6a4c631d7d
ZDing <kdfluxit@hotmail.com>
2018-07-30 10:37:07 +0800

    test features in c++

---
+++ myxcodetest/myxcodetest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  myxcodetest
+//
+//  Created by 丁中航 on 2018/7/30.
+//  Copyright © 2018 SJTu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
